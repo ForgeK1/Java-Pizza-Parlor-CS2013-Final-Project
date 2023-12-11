c853dd77c160a104787fba36a39091080446ec62
JacksonLe71 <jacksonle717@gmail.com>
2023-12-10 22:11:47 -0800

    Needle in the Haystack minigame

---
+++ CS 2013 Final Project/src/Minigames/NeedleInTheHaystackTest.java
@@ -0,0 +1,8 @@
+package Minigames;
+
+public class NeedleInTheHaystackTest {
+    public static void main(String args[]) {
+        NeedleInTheHaystack game = new NeedleInTheHaystack();
+        game.play();
+    }
+}
